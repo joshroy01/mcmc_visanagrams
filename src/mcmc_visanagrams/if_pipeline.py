854b09e35230d371445ce1cbbf72fd753cdbb0e5
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-11 10:47:29 -0400

    Added option for switching off edge interpolation in make_canvas
    - Also warns about interpolation issues I've seen

--- src/mcmc_visanagrams/if_pipeline.py
+++ src/mcmc_visanagrams/if_pipeline.py
@@ -122,7 +122,8 @@ def make_canvas(latents,
                 sizes,
                 in_channels=3,
                 base_size=64,
-                views: Optional[List['BaseView']] = None):
+                views: Optional[List['BaseView']] = None,
+                do_edge_interpolation: bool = True):
     # Make a canvas from different latents
     canvas_count = torch.zeros(canvas_size, canvas_size).to(latents.device)
     canvas_latent = torch.zeros(1, in_channels, canvas_size, canvas_size,
@@ -132,20 +133,23 @@ def make_canvas(latents,
         latent = latent[None]
         sf, x_start, y_start = size
         size = min(canvas_size - x_start, base_size) * sf
+
         latent_expand = interpolate(latent, (size, size), mode='nearest')
 
         if views:
             latent_expand = views[i].inverse_view(latent_expand)
 
         weight = 1 / (sf**2)
-        coords = torch.linspace(-1, 1, size).to(latents.device)
-        XX, YY = torch.meshgrid(coords, coords)
-        dist_from_edge = torch.sqrt(torch.min((1 - torch.abs(XX))**2, (1 - torch.abs(YY))**2))
-        dist_from_edge = torch.minimum(
-            torch.tensor(6 / 32), dist_from_edge
-        )  # only fall off along the outer edge, to avoid sharp lines at edge of each contributing image
-        dist_from_edge = dist_from_edge / torch.max(dist_from_edge)
-        weight = weight * dist_from_edge + 1e-6
+        if do_edge_interpolation:
+            coords = torch.linspace(-1, 1, size).to(latents.device)
+            XX, YY = torch.meshgrid(coords, coords)
+            dist_from_edge = torch.sqrt(torch.min((1 - torch.abs(XX))**2, (1 - torch.abs(YY))**2))
+            dist_from_edge = torch.minimum(
+                torch.tensor(6 / 32), dist_from_edge
+            )  # only fall off along the outer edge, to avoid sharp lines at edge of each contributing image
+            dist_from_edge = dist_from_edge / torch.max(dist_from_edge)
+            weight = weight * dist_from_edge
+        weight = weight + 1e-6
 
         canvas_latent[:, :, y_start:y_start + size, x_start:x_start +
                       size] = weight * latent_expand + canvas_latent[:, :, y_start:y_start + size,
@@ -798,6 +802,12 @@ class IFPipeline(DiffusionPipeline, LoraLoaderMixin):
         sizes, prompt, weights, views = context.collapse()
         device = self._execution_device
         weights = torch.Tensor(weights).to(device)[:, None, None, None]
+
+        if len(set(sizes)) != len(sizes):
+            # This conditional means that we have two views corresponding to the same sub-image
+            # (indexed by size).
+            warn("Warning! Dylan noticed that edge interpolation in make_canvas when producing "
+                 "visual anagrams can lead to poor results. Might want to turn this off.")
 
         # 1. Check inputs. Raise error if not correct
         self.check_inputs(prompt, callback_steps, negative_prompt, prompt_embeds,

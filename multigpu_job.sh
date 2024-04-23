2113f027323f5e2d68b9552713c5a007e70ef517
Dylan Colli <dylanfrankcolli@gmail.com>
2024-04-23 03:17:24 -0400

    Warn about usage of the multi-gpu job runner
    - I believe that spawning multiple doit processes to hack multi-gpu utilization created a race condition and corrupted my database for doit. Warn about this in the future in case I try to run it again without debugging the true cause.

--- multigpu_job.sh
+++ multigpu_job.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "WARNING: I think this job is unsafe to run and corrupts the doit"
+echo "    database due to a race condition!"
+
 # The original pipeline isn't setup (to my knowledge) for multi-gpu runs. So instead, we specify via
 # command line what GPUs are available per job. Hacky, but boy does it work.
-CUDA_VISIBLE_DEVICES=0 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed0
-CUDA_VISIBLE_DEVICES=1 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed90210
-CUDA_VISIBLE_DEVICES=2 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed8675309
+# CUDA_VISIBLE_DEVICES=0 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed0
+# CUDA_VISIBLE_DEVICES=1 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed90210
+# CUDA_VISIBLE_DEVICES=2 conda run -n mcmc_visanagrams doit -f multirun_jobs/dodo.py *seed8675309
